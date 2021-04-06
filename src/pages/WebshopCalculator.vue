038505f056b2692cf9441000ce26c3d7a790f525
martijn <martijn.brug@gmail.com>
2021-04-06 09:07:32 +0200

    Working on calculator

--- src/pages/WebshopCalculator.vue
+++ src/pages/WebshopCalculator.vue
@@ -1,37 +1,84 @@
 <template>
   <DefaultLayout>
 
-    <section id="webshop-calculator" class="section" >
-    <div class="container" >
-    <br>
-    <br>
-    <br>
-    <br>
-    <h1 class="title"> Werk aan de winkel.... </h1>
-    <p>We zijn er mee bezig, binnenkort kun je hier berekenen wat een website voor jou gaat kosten.</p>
-    <br>
-    <br>
-    <br>
-    <br>
-    <b-button
-        label="Vraag alvast meer informatie aan"
-        type="is-info"
-        size="is-medium"
-        @click="isModalActive = true"/>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    </div>
+    <section id="webshop-calculator" class="section">
+      <div class="container">
+        <br>
+        <br>
+        <h1 class="title"> Webshop calculator</h1>
+        <p class="subtitle">Wat kost een webshop bij Pinelab?</p>
+        <p>Wat je ook kiest, dit krijg je altijd:</p>
+        <br>
+        <div class="columns">
+          <div class="column checklist">
+
+            <table>
+              <tr>
+                <td><b-icon type="is-success" icon="check"></b-icon></td>
+                <td>Razendsnelle static site</td>
+              </tr>
+              <tr>
+                <td><b-icon type="is-success" icon="check"></b-icon></td>
+                <td>SEO vriendelijk</td>
+              </tr>
+              <tr>
+                <td><b-icon type="is-success" icon="check"></b-icon></td>
+                <td>Persoonlijke support</td>
+              </tr>
+            </table>
+
+          </div>
+          <div class="column checklist">
+            <table>
+              <tr>
+                <td><b-icon type="is-success" icon="check"></b-icon></td>
+                <td>Responsive, dus geschikt voor mobiel</td>
+              </tr>
+              <tr>
+                <td><b-icon type="is-success" icon="check"></b-icon></td>
+                <td>Makkelijk zelf producten beheren zonder technische kennis</td>
+              </tr>
+              <tr>
+                <td><b-icon type="is-success" icon="check"></b-icon></td>
+                <td>De mogelijkheid om zelf Instagram shopping te koppelen</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+
+        <h2 class="title">1. Welke layout wil je?</h2>
+
+        <p :hidden="layout !== 'minimal'"><strong>Minimal layout</strong> is een strakke, minimale layout die de focus legt op je producten. Deze
+          layout is geschikt voor shops tot ~50 producten.</p>
+        <p :hidden="layout !== 'custom'"><strong>Custom layout</strong> is een uitgebreidere layout voor grotere shops met meerdere categorieën. We
+          bepalen samen welke onderdelen er in je webshop komen.</p>
+        <br>
+        <b-field :oninput="calculate()">
+          <b-radio-button v-model="layout"
+                          native-value="minimal"
+                          type="is-success is-light is-outlined">
+            <b-icon icon="close"></b-icon>
+            <span>Minimal layout</span>
+          </b-radio-button>
+
+          <b-radio-button v-model="layout"
+                          native-value="custom"
+                          type="is-success is-light is-outlined">
+            <b-icon icon="check"></b-icon>
+            <span>Custom layout</span>
+          </b-radio-button>
+        </b-field>
+
+        <br>
+        <br>
+        <br>
+
+        <b-button
+            label="Vraag gratis voorbeeld aan"
+            type="is-info"
+            size="is-medium"
+            @click="isModalActive = true"/>
+      </div>
     </section>
 
 
@@ -64,7 +111,18 @@ export default {
   data() {
     return {
       isModalActive: false,
+      layout: 'minimal'
+    }
+  },
+  methods: {
+    calculate() {
+      console.log(this.layout)
     }
   }
 }
 </script>
+<style>
+.checklist .icon {
+  margin-right: 20px;
+}
+</style>
