0e57bae66564ea5b22c4b1c4eae4f610e289a4d4
martijn <martijn.brug@gmail.com>
2021-04-07 11:11:27 +0200

    Added beheer pagina with link to videos

---
+++ src/pages/BeherenVanJeWebshop.vue
@@ -0,0 +1,31 @@
+<template>
+  <DefaultLayout>
+
+    <section id="404" class="section">
+      <div class="container">
+        <br>
+        <br>
+        <br>
+        <br>
+        <h1 class="title">Beheren van je webshop</h1>
+        <br>
+        <br>
+        <p><a href="https://www.youtube.com/channel/UCYPTytLzrZA8qhpO3tDds2w" target="_blank">Hier</a> kun je video's vinden over het toevoegen van producten, collecties en het afhandelen van bestellingen in je webshop.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      </div>
+    </section>
+  </DefaultLayout>
+</template>
+<script>
+
+export default {};
+</script>
